c0cf8e21a80e97287e71bd77fd53af63bc66f864
bouncei <joshuainyang255@gmail.com>
2022-02-04 16:21:04 +0100

    connect wallet for authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg t`,
+}
+
+const supportedChainIds = [4]
+const connectors = {
+  injected: {},
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider supportedChainIds={supportedChainIds} connectors={connectors}>
+
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+    
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,49 @@
+import { useWeb3 } from '@3rdweb/hooks'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
 
-export default function Home() {
-  return (
-    <>
-      <Header/>
-      <Hero/>
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+}
 
-    </>
+export default function Home() {
+  const {address, connectWallet} = useWeb3()
+  return (
+    <div className={style.wrapper}>
+      {address ?(   //Conditional Rendering
+
+        <>
+          <Header/>
+          <Hero/>
+
+        </>
+
+      ) : (
+
+        <div className={style.walletConnectWrapper}>
+        <button
+         className={style.button}
+         onClick={() => connectWallet("injected")}  //Prompts User to connect their metamask wallet
+         
+        >
+          Connect Wallet
+
+        </button>
+
+        <div className={style.details}>
+          Please connect your metamask wallet
+
+        </div>
+
+        </div>
+        
+      )}  
+    </div>
     
   )
 }
