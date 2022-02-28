ccf4dfeeb6577dea7777947767adedae160d6920
bouncei <joshuainyang255@gmail.com>
2022-02-28 03:46:57 +0100

    home cards

---
+++ components/HomeCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { sanityClient } from '../sanity';
+
+const HomeCard = (({ profileImage, title, description }) =>{
+    return (
+        <div></div>
+    )
+})
+
+
+
+
+export default HomeCard
