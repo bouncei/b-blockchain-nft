49b0dd73ed6f8b1b0d472db755d4f55b30cfebd8
bouncei <joshuainyang255@gmail.com>
2022-04-07 23:14:06 +0100

    hhh

---
+++ pages/nnn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function nnn() {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-center text-3xl">DETAILS</h1>
+      <p>sdhbcbashdckahdcahvdj</p>
+    </div>
+  )
+}
