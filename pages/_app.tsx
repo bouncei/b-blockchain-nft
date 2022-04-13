cddf5b58b72a10d23fc5fa672380f1e255ea8b6b
bouncei <joshuainyang255@gmail.com>
2022-04-13 22:56:02 +0100

    show preloader on page change with useRouter() hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,30 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 // import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 import { TransactionProvider } from '../context/TransactionContext'
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import Router from 'next/router';
+// import Nprogress from 'nprogress/';
 
-// const supportedChainIds = [4]
-// const connectors = {
-//   injected: {},
-// }
+
+
+
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    
+
     // <ThirdwebWeb3Provider supportedChainIds={supportedChainIds} connectors={connectors}>
 
     //   <Component {...pageProps} />
     // </ThirdwebWeb3Provider>
-    
+
     <TransactionProvider>
       <Component {...pageProps} />
     </TransactionProvider>
