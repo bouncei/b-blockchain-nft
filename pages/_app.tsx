2e5dc5b9403c4b15747455f4fcb0efdbb7515f5c
bouncei <joshuainyang255@gmail.com>
2022-04-20 22:36:28 +0100

    another commt to the frontend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,21 +2,21 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 // import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
 import { TransactionProvider } from '../context/TransactionContext'
-// import NProgress from 'nprogress';
-// import 'nprogress/nprogress.css';
-// import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import Router from 'next/router';
 import Head from 'next/head';
 
 
 
 
-// Router.events.on('routeChangeStart', (url) => {
-//   console.log(`Loading: ${url}`);
-//   NProgress.start();
-// });
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
 
-// Router.events.on('routeChangeComplete', () => NProgress.done());
-// Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
